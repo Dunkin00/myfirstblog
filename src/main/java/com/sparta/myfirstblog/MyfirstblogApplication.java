e11be4d578daa3e471a05d77ee6346e252ef1e78
Dunkin00 <meister630@gmail.com>
2023-04-17 12:00:24 +0900

    Initial commit
    myfirstblog 프로젝트 생성
    모든게 처음이라 항상 first가 들어간다
    재밌다

---
+++ src/main/java/com/sparta/myfirstblog/MyfirstblogApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.myfirstblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyfirstblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/myfirstblog/MyfirstblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.myfirstblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
