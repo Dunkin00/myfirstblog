d7245ea1ed867b1fc4e6ccfb25542d7caa34e27f
Dunkin00 <meister630@gmail.com>
2023-04-21 12:17:44 +0900

    회원가입시 username, password 길이제한 추가

--- src/main/java/com/sparta/myfirstblog/controller/UserController.java
+++ src/main/java/com/sparta/myfirstblog/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,7 +19,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public String signup(@RequestBody SignupRequestDto signupRequestDto){
+    public String signup(@Valid @RequestBody SignupRequestDto signupRequestDto){
         return userService.signup(signupRequestDto);
     }
 
